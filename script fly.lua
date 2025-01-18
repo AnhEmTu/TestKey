daf997458b89bbfbfd5d349ec16be87e75b38257
kobot <164139791+AnhEmTu@users.noreply.github.com>
2025-01-18 15:46:44 +0700

    Add files via upload

--- script fly.lua
+++ script fly.lua
@@ -146,6 +146,15 @@ Frame.Draggable = true
 
 onof.MouseButton1Down:connect(function()
 
+-- Phát nhạc khi mã chạy (ID nhạc khi chạy mã là 7817341182)
+local joinSound = Instance.new("Sound")
+joinSound.Name = "JoinSound"
+joinSound.SoundId = "rbxassetid://140910211" -- ID nhạc khi chạy mã
+joinSound.Volume = 0.3
+joinSound.Looped = false
+joinSound.Parent = workspace
+joinSound:Play()
+
     if nowe == true then
         nowe = false
 
